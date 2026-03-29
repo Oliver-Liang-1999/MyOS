57f9cede7f77215f1a50b105252c98011ef26137
Oliver-Liang-1999 <156862365+Oliver-Liang-1999@users.noreply.github.com>
2026-03-29 21:37:00 +1100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'myos-v2';
+const CORE  = [
+  'hub.html',
+  'meals-2.html',
+  'reservoir-1.html',
+  'bookshelf.html',
+  'manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }))
+  );
+});
